6339fef3be28739515bddc12cb78d8bd69c6cd65
Kelebek1 <eeeedddccc@hotmail.co.uk>
2020-08-29 20:37:19 +0100

    Data too stronk

--- src/overlays/actors/ovl_En_Horse_Game_Check/z_en_horse_game_check.c
+++ src/overlays/actors/ovl_En_Horse_Game_Check/z_en_horse_game_check.c
@@ -4,6 +4,7 @@
  * Description: Horseback Minigames
  */
 
+#include "overlays/actors/ovl_En_Horse/z_en_horse.h"
 #include "z_en_horse_game_check.h"
 
 #define FLAGS 0x00000010
@@ -15,7 +16,23 @@ void EnHorseGameCheck_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void EnHorseGameCheck_Update(Actor* thisx, GlobalContext* globalCtx);
 void EnHorseGameCheck_Draw(Actor* thisx, GlobalContext* globalCtx);
 
-/*
+
+s32 func_80A67550(EnHorseGameCheck *this, GlobalContext *globalCtx);
+s32 func_80A67BDC(EnHorseGameCheck *this, GlobalContext *globalCtx);
+s32 func_80A67C50(EnHorseGameCheck *this, GlobalContext *globalCtx);
+s32 func_80A67C88(EnHorseGameCheck *this, GlobalContext *globalCtx);
+
+void func_80A67608(EnHorseGameCheck *this, GlobalContext *globalCtx);
+void func_80A67BF8(EnHorseGameCheck *this, GlobalContext *globalCtx);
+void func_80A67C68(EnHorseGameCheck *this, GlobalContext *globalCtx);
+void func_80A67CD4(EnHorseGameCheck *this, GlobalContext *globalCtx);
+
+void func_80A67764(EnHorseGameCheck *this, GlobalContext *globalCtx);
+void func_80A67C08(EnHorseGameCheck *this, GlobalContext *globalCtx);
+void func_80A67C78(EnHorseGameCheck *this, GlobalContext *globalCtx);
+void func_80A67DFC(EnHorseGameCheck *this, GlobalContext *globalCtx);
+
+
 const ActorInit En_Horse_Game_Check_InitVars = {
     ACTOR_EN_HORSE_GAME_CHECK,
     ACTORTYPE_BG,
@@ -27,39 +44,269 @@ const ActorInit En_Horse_Game_Check_InitVars = {
     (ActorFunc)EnHorseGameCheck_Update,
     (ActorFunc)EnHorseGameCheck_Draw,
 };
-*/
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67550.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67608.s")
+Vec3f D_80A68440[] = {
+    { 1700.0f, 1.0f, -540.0f },
+    { 117.0f, 1.0f, 488.0f },
+    { -1700.0f, 1.0f, -540.0f },
+};
+
+EnHorseGameCheckFuncs D_80A684F0[] = {
+    NULL,
+    func_80A67550,
+    func_80A67BDC,
+    func_80A67C50,
+    func_80A67C88,
+};
+
+EnHorseGameCheckActionFunc D_80A68504[] = {
+    NULL,
+    func_80A67608,
+    func_80A67BF8,
+    func_80A67C68,
+    func_80A67CD4,
+};
+
+EnHorseGameCheckActionFunc D_80A68518[] = {
+    NULL,
+    func_80A67764,
+    func_80A67C08,
+    func_80A67C78,
+    func_80A67DFC,
+};
+
+s32 func_80A67550(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+    EnHorse *horse;
+    s32 i;
+
+    this->unk_14C = 1;
+    this->unk_150[0] = 0;
+    for (i = 0; i < 3; i++) {
+        this->unk_150[i + 1] = 0;
+    }
+
+    horse = (EnHorse*)Actor_Spawn(&globalCtx->actorCtx, globalCtx, ACTOR_EN_HORSE, -250.0f, 1.0f, -1650.0f, 0, 0x4000, 0, -0x7FFD);
+    this->unk_16C = horse;
+    if (horse == NULL) {
+        LogUtils_HungupThread("../z_en_horse_game_check.c", 0x181);
+    }
+
+    this->unk_170 = 0;
+    this->unk_178 = 0;
+    this->unk_174 = 0;
+    this->unk_17C = 0;
+    this->unk_180 = 0;
+    return 1;
+}
+
+void func_80A67608(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67618.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67764.s")
+void func_80A67764(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+    Player* player = PLAYER;
+    s32 i;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67BDC.s")
+    if ((this->unk_170 >= 0x33) && (this->unk_150.unk_00 & 2) == 0) {
+        this->unk_150.unk_00 |= 2;
+        func_80088B34(0);
+    } else if ((this->unk_170 >= 0x51) && (player->rideActor != NULL) && ((this->unk_150.unk_00 & 1) == 0)) {
+        this->unk_150.unk_00 |= 1;
+        ((EnHorse*)player->rideActor)->unk_388 = 1;
+    } else if ((this->unk_170 >= 0x52) && ((this->unk_150.unk_00 & 4) == 0)) {
+        this->unk_16C->unk_388 = 1;
+        this->unk_150.unk_00 |= 4;
+        Audio_PlaySoundGeneral(NA_SE_SY_START_SHOT, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
+    }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67BF8.s")
+    this->unk_170++;
+
+    for (i = 0; i < 3; i++) {
+        if (player->rideActor != NULL) {
+            if (Math3D_Vec3f_DistXYZ(&D_80A68440[i], &player->rideActor->posRot.pos) < 400.0f) {
+                if ((i > 0) && (1 == this->unk_150[i])) {
+                    this->unk_150[i].unk_04 = 1;
+                } else if (i == 0) {
+                    this->unk_150[i].unk_04 = 1;
+                }
+            }
+        }
+
+        if (Math3D_Vec3f_DistXYZ(&D_80A68440[i], &this->unk_16C->actor.posRot.pos) < 400.0f) {
+            if ((i > 0) && (this->unk_150.unk_C[i] == 1)) {
+                this->unk_150.unk_C[i+1] = 1;
+            } else if (i == 0) {
+                this->unk_150.unk_C[i+1] = 1;
+            }
+        }
+    }
+
+    if (this->unk_174 == 0) {
+        temp_v0_4 = player->->unk440;
+        if (temp_v0_4 != 0) {
+            if (1 == this->unk15C) {
+                if (Math3D_PointInSquare2D(D_80A68464.unk0, D_80A68464.unk4, (bitwise f32) D_80A68464.unk8, (bitwise f32) D_80A68464.unkC, temp_v0_4->unk24, temp_v0_4->unk2C) != 0) {
+                    temp_t0 = this->unk_17C + 1;
+                    this->unk_17C = temp_t0;
+                    if (temp_t0 > 0) {
+                        this->unk_174 = 1;
+                        this->unk_178 = 0x37;
+                        Audio_SetBGM(0x41);
+                        Audio_PlaySoundGeneral(0x4835, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
+                    }
+                    phi_v0 = this;
+                    phi_s0_2 = 0;
+loop_33:
+                    temp_s0_2 = phi_s0_2 + 1;
+                    temp_v0_5 = phi_v0 + 4;
+                    temp_v0_5->unk_150 = 0;
+                    phi_v0 = temp_v0_5;
+                    phi_s0_2 = temp_s0_2;
+                    if (temp_s0_2 != 3) {
+                        goto loop_33;
+                    }
+                }
+            }
+        }
+        temp_v0_6 = this->unk_16C;
+        if (temp_v0_6 != 0) {
+            if (1 == this->unk168) {
+                if (Math3D_PointInSquare2D(D_80A68464.unk0, D_80A68464.unk4, (bitwise f32) D_80A68464.unk8, (bitwise f32) D_80A68464.unkC, temp_v0_6->actor.posRot.pos.x, temp_v0_6->actor.posRot.pos.z) != 0) {
+                    temp_t6 = this->unk_180 + 1;
+                    this->unk_180 = temp_t6;
+                    if (temp_t6 > 0) {
+                        temp_v0_7 = this->unk_16C;
+                        this->unk_174 = 2;
+                        this->unk_178 = 0x46;
+                        temp_v0_7->unk1F0 = temp_v0_7->unk1F0 | 0x800000;
+                        Audio_SetBGM(0x41);
+                        Audio_PlaySoundGeneral(0x4835, &D_801333D4, 4, &D_801333E0, &D_801333E0, &D_801333E8);
+                    }
+                    phi_v0_2 = this;
+                    phi_s0_3 = 0;
+loop_40:
+                    temp_s0_3 = phi_s0_3 + 1;
+                    temp_v0_8 = phi_v0_2 + 4;
+                    temp_v0_8->unk15C = 0;
+                    phi_v0_2 = temp_v0_8;
+                    phi_s0_3 = temp_s0_3;
+                    if (temp_s0_3 != 3) {
+                        goto loop_40;
+                    }
+                }
+            }
+        }
+        temp_v0_9 = player->->unk440;
+        if ((temp_v0_9 == 0) || (Math3D_PointInSquare2D(D_80A68474.unk0, D_80A68474.unk4, (bitwise f32) D_80A68474.unk8, (bitwise f32) D_80A68474.unkC, temp_v0_9->unk24, temp_v0_9->unk2C) == 0)) {
+            if (Math3D_PointInSquare2D(D_80A68474.unk0, D_80A68474.unk4, (bitwise f32) D_80A68474.unk8, (bitwise f32) D_80A68474.unkC, player->->unk24, player->->unk2C) != 0) {
+block_44:
+                Audio_SetBGM(0x41);
+                this->unk_174 = 2;
+                this->unk_178 = 0x14;
+            }
+        } else {
+            goto block_44;
+        }
+        if (gSaveContext.timer1Value >= 0xB4) {
+            if ((this->unk_150 & 2) != 0) {
+                Audio_SetBGM(0x41);
+                this->unk_174 = 3;
+                this->unk_178 = 0x14;
+            }
+        }
+    } else {
+        temp_v0_10 = this->unk_178;
+        if (temp_v0_10 > 0) {
+            this->unk_178 = temp_v0_10 - 1;
+        } else {
+            func_80A67618(this, globalCtx);
+        }
+    }
+}
+
+s32 func_80A67BDC(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+    this->unk_14C = 2;
+    this->unk_150[0] = 0;
+    this->unk_150[1] = 0;
+    return 1;
+}
+
+void func_80A67BF8(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67C08.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67C50.s")
+s32 func_80A67C50(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+    this->unk_14C = 3;
+    this->unk_150[0] = 0;
+    return 1;
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67C68.s")
+void func_80A67C68(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67C78.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67C88.s")
+#ifdef NON_MATCHING
+s32 func_80A67C88(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+    s32 i;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67CD4.s")
+    this->unk_14C = 4;
+    this->unk_150[1] = 0;
+    this->unk_198 = 0;
+    this->unk_19C = 0;
+    for (i = 0; i < 4; i++) {
+        this->unk_158[i][0] = this->unk_158[i][1] = this->unk_158[i][2] = this->unk_158[i][3] = 0;
+    }
+
+    this->unk_1A0 = 0;
+    return 1;
+}
+#else
+#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67C88.s")
+#endif
+
+void func_80A67CD4(EnHorseGameCheck *this, GlobalContext *globalCtx) {
+
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67CE4.s")
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/func_80A67DFC.s")
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/EnHorseGameCheck_Init.s")
+void EnHorseGameCheck_Init(Actor* thisx, GlobalContext *globalCtx) {
+    EnHorseGameCheck *this = THIS;
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/EnHorseGameCheck_Destroy.s")
+    if ((globalCtx->sceneNum == SCENE_SPOT20) &&
+        (Flags_GetEventChkInf(0x18) || (DREG(1) != 0))) {
+        this->actor.params = 4;
+    }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/EnHorseGameCheck_Update.s")
+    if (D_80A684F0[this->actor.params] != NULL) {
+        D_80A684F0[this->actor.params](this, globalCtx);
+    }
+}
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/actors/ovl_En_Horse_Game_Check/EnHorseGameCheck_Draw.s")
+void EnHorseGameCheck_Destroy(Actor* thisx, GlobalContext *globalCtx) {
+    EnHorseGameCheck *this = THIS;
+
+    if (D_80A68504[this->actor.params] != NULL) {
+        D_80A68504[this->actor.params](this, globalCtx);
+    }
+}
+
+void EnHorseGameCheck_Update(Actor* thisx, GlobalContext *globalCtx) {
+    EnHorseGameCheck *this = THIS;
+
+    if (D_80A68518[this->unk_14C] != NULL) {
+        D_80A68518[this->unk_14C](this, globalCtx);
+    }
+}
+
+void EnHorseGameCheck_Draw(Actor* thisx, GlobalContext *globalCtx) {
+
+}
--- src/overlays/actors/ovl_En_Horse_Game_Check/z_en_horse_game_check.h
+++ src/overlays/actors/ovl_En_Horse_Game_Check/z_en_horse_game_check.h
@@ -6,9 +6,31 @@
 
 struct EnHorseGameCheck;
 
+typedef void (*EnHorseGameCheckActionFunc)(struct EnHorseGameCheck*, GlobalContext*);
+typedef s32 (*EnHorseGameCheckFuncs)(struct EnHorseGameCheck*, GlobalContext*);
+
+typedef struct {
+    s32 unk_00;
+    s32 unk_04;
+    s32 unk_08;
+    s32 unk_0C[3];
+} EnHorseGameCheckStruct1;
+
 typedef struct EnHorseGameCheck {
     /* 0x0000 */ Actor actor;
-    /* 0x014C */ char unk_14C[0x58];
+    /* 0x014C */ s32 unk_14C;
+    /* 0x0150 */ EnHorseGameCheckStruct1 unk_150;
+    /* 0x0168 */ char unk_168[0x4];
+    /* 0x016C */ EnHorse* unk_16C;
+    /* 0x0170 */ s32 unk_170;
+    /* 0x0174 */ s32 unk_174;
+    /* 0x0178 */ s32 unk_178;
+    /* 0x017C */ s32 unk_17C;
+    /* 0x0180 */ s32 unk_180;
+    /* 0x0184 */ char unk_184[0x14];
+    /* 0x0198 */ s32 unk_198;
+    /* 0x019C */ s32 unk_19C;
+    /* 0x01A0 */ s32 unk_1A0;
 } EnHorseGameCheck; // size = 0x01A4
 
 extern const ActorInit En_Horse_Game_Check_InitVars;
